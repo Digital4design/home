3cf29a7ca0b7a2061014c4a703edc805caf0d475
Joe <joe@creativefolks.co.uk>
2022-03-08 21:59:55 +0000

    fix overflow bug for safari browser. Increase hitbox of hamburger menu

--- components/Layout/MobileMenu/Hamburger.tsx
+++ components/Layout/MobileMenu/Hamburger.tsx
@@ -5,22 +5,22 @@ export default function Hamburger() {
   const { isOpen, toggleOpen } = useContext(MobileMenuContext)
 
   const commonClasses =
-    "pointer-events-none h-0.5 bg-brand-blue transition-all duration-150 ease-in-out"
+    "pointer-events-none h-0.5 bg-brand-blue transition-all duration-200 ease-in-out"
 
   return (
     <div
-      className="group ml-auto flex h-5 w-5 cursor-pointer flex-col items-end justify-between lg:hidden"
+      className="group ml-auto flex h-14 w-14 cursor-pointer flex-col items-end justify-between p-4 lg:hidden"
       onClick={toggleOpen}
     >
       <span
         className={`w-full ${commonClasses} ${
-          isOpen && "translate-y-2 rotate-45"
+          isOpen && "translate-y-3 rotate-45"
         }`}
       />
       <span className={`${commonClasses} w-full ${isOpen && "opacity-0"}`} />
       <span
         className={`${commonClasses} ${
-          isOpen ? "w-full -translate-y-2.5 -rotate-45" : "w-3"
+          isOpen ? "w-full -translate-y-2.5 -rotate-45" : "w-2/3"
         }`}
       />
     </div>
