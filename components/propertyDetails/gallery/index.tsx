edef8e6ef2a60a3ad918c21ccdc63c3fe678b7b2
Brendan <macbook@MBP.local>
2022-03-12 21:25:57 +0000

    swiper update

--- components/propertyDetails/gallery/index.tsx
+++ components/propertyDetails/gallery/index.tsx
@@ -1,10 +1,8 @@
-import SwiperCore, { EffectCoverflow, Pagination } from "swiper";
+import { Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 
 function Gallery() {
-  SwiperCore.use([EffectCoverflow, Pagination]);
-
   const slide_img = [
     "https://www.taylorwimpey.co.uk/-/twdxmedia/images/shared/product/four-bedroom-homes/the-monro/twes_albany-grange_dunbar_monro.jfif?mw=1438&hash=CC692011314B408FAAC86ABB32E97000",
     "https://www.taylorwimpey.co.uk/-/twdxmedia/images/national/modules/article-hero/exteriors/twny_wynyard-manor_wynyard.jpg?mh=1200&la=en&h=1200&w=1800&mw=1800&hash=0D5885383DDDA0AB3ECF0814936ACCF0",
@@ -12,8 +10,11 @@ function Gallery() {
   return (
     <div className="relative">
       <Swiper
+        pagination={{
+          type: "bullets",
+        }}
+        modules={[Pagination]}
         slidesPerView={"auto"}
-        pagination={true}
         className="h-[600px] rounded-tl rounded-tr"
         loop={true}
       >
