4ca6f15c221bdd4d16d1ecf7cf7f7ae4018580eb
Brendan <macbook@MBP.local>
2022-03-12 21:17:19 +0000

    added swiper

--- components/propertyDetails/gallery/index.tsx
+++ components/propertyDetails/gallery/index.tsx
@@ -1,9 +1,32 @@
-import React, { useRef, useState } from "react";
-// Import Swiper React components
+import SwiperCore, { EffectCoverflow, Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 function Gallery() {
-  return <div className=" h-[600px] rounded bg-brand-green"></div>;
+  SwiperCore.use([EffectCoverflow, Pagination]);
+
+  const slide_img = [
+    "https://www.taylorwimpey.co.uk/-/twdxmedia/images/shared/product/four-bedroom-homes/the-monro/twes_albany-grange_dunbar_monro.jfif?mw=1438&hash=CC692011314B408FAAC86ABB32E97000",
+    "https://www.taylorwimpey.co.uk/-/twdxmedia/images/national/modules/article-hero/exteriors/twny_wynyard-manor_wynyard.jpg?mh=1200&la=en&h=1200&w=1800&mw=1800&hash=0D5885383DDDA0AB3ECF0814936ACCF0",
+  ];
+  return (
+    <div className="relative">
+      <Swiper
+        slidesPerView={"auto"}
+        pagination={true}
+        className="h-[600px] rounded-tl rounded-tr"
+        loop={true}
+      >
+        {slide_img.map((img, i) => {
+          return (
+            <SwiperSlide key={i}>
+              <img src={img} alt="" />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
 }
 
 export default Gallery;
