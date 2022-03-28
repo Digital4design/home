2fa15faf16c4740bb2e41826ee100facdc809e62
Brendan <macbook@MBP.local>
2022-03-28 11:35:02 +0100

    property gallery updates

--- components/propertyDetails/gallery/index.tsx
+++ components/propertyDetails/gallery/index.tsx
@@ -15,12 +15,8 @@ function Gallery() {
     "https://www.taylorwimpey.co.uk/-/twdxmedia/images/shared/product/four-bedroom-homes/the-monro/twes_albany-grange_dunbar_monro.jfif?mw=1438&hash=CC692011314B408FAAC86ABB32E97000",
     "https://www.taylorwimpey.co.uk/-/twdxmedia/images/national/modules/article-hero/exteriors/twny_wynyard-manor_wynyard.jpg?mh=1200&la=en&h=1200&w=1800&mw=1800&hash=0D5885383DDDA0AB3ECF0814936ACCF0",
     "https://www.taylorwimpey.co.uk/-/twdxmedia/images/national/sales-and-marketing/house-types/the-coltham/taylor-wimpey-the-coltham-4-bed-home-01.jpg?mw=1438&hash=7766BABAED6A92F8A0E5975332B058BD",
+    "https://www.taylorwimpey.co.uk/-/twdxmedia/images/national/sales-and-marketing/house-types/the-coltham/taylor-wimpey-the-coltham-4-bed-home-01.jpg?mw=1438&hash=7766BABAED6A92F8A0E5975332B058BD",
   ];
-
-  const styles = {
-    "--swiper-navigation-color": "#fff",
-    "--swiper-pagination-color": "#fff",
-  };
 
   return (
     <div className="relative">
@@ -29,13 +25,12 @@ function Gallery() {
           type: "bullets",
         }}
         navigation
-        style={styles}
-        spaceBetween={10}
+        spaceBetween={20}
         thumbs={{ swiper: thumbsSwiper }}
         modules={[FreeMode, Navigation, Pagination, Thumbs]}
         slidesPerView={"auto"}
         centeredSlides
-        className="rounded-tl rounded-tr"
+        className="product-images-slider rounded-tl rounded-tr"
         loop
         grabCursor
       >
@@ -49,26 +44,25 @@ function Gallery() {
           );
         })}
       </Swiper>
-      <div className="mt-2">
-        <Swiper
-          onSwiper={setThumbsSwiper}
-          spaceBetween={10}
-          slidesPerView={4}
-          freeMode={true}
-          watchSlidesProgress={true}
-          modules={[FreeMode, Navigation, Thumbs]}
-        >
-          {slide_img.map((img, index) => {
-            return (
-              <SwiperSlide key={index}>
-                <figure className="relative h-[150px]">
-                  <Image src={img} alt="" layout="fill" objectFit="cover" />
-                </figure>
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-      </div>
+
+      <Swiper
+        spaceBetween={20}
+        slidesPerView={5}
+        freeMode={true}
+        watchSlidesProgress={true}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="product-images-slider-thumbs"
+      >
+        {slide_img.map((img, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <figure className="relative mt-4 h-[150px]">
+                <Image src={img} alt="" layout="fill" objectFit="cover" />
+              </figure>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </div>
   );
 }
