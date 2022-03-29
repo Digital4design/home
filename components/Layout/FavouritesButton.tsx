86b32f49635a8b909064dd77dae5b28ac6279fc6
Joe <joe@creativefolks.co.uk>
2022-03-29 12:47:10 +0100

    fix tooltip rebuild on refresh

--- components/Layout/FavouritesButton.tsx
+++ components/Layout/FavouritesButton.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 export default function FavouritesButton() {
   return (
     <Link href="/favourites">
-      <a className=" text-brand-blue hover:text-brand-green">
+      <a className="flex text-brand-blue hover:text-brand-green">
         <HeartIcon className="h-6 w-6" />
         <span className="ml-2 lg:hidden">Favourites</span>
       </a>
