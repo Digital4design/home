33a8fc3ad6d57144606969960100470412d53fab
Joe <joe@creativefolks.co.uk>
2022-03-30 11:32:43 +0100

    fix tailwind build error

--- components/SearchAndFilters/FilterSection/index.tsx
+++ components/SearchAndFilters/FilterSection/index.tsx
@@ -10,7 +10,7 @@ export default function FilterSection({ children, onClick, width }: Props) {
   return (
     <div
       className={`group relative flex h-full cursor-pointer flex-col justify-center border-r border-white px-8 text-left text-white ${
-        width ? `w-[${width}px]` : "w-auto"
+        width ? `w-[${String(width)}px]` : "w-auto"
       }`}
       onClick={onClick}
     >
