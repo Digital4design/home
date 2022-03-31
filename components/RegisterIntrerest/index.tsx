90fadea38267c79e8c9d54269a243a1e19e98b66
Brendan <macbook@MBP.local>
2022-03-31 09:13:55 +0100

    form fields

--- components/RegisterIntrerest/index.tsx
+++ components/RegisterIntrerest/index.tsx
@@ -3,6 +3,8 @@ import { useForm, SubmitHandler } from "react-hook-form";
 type Inputs = {
   firstName: string;
   lastName: string;
+  email: string;
+  phone: number;
 };
 
 function RegisterInterest() {
@@ -16,26 +18,49 @@ function RegisterInterest() {
 
   return (
     <div className="absolute top-0 left-0 z-999 flex h-full w-full items-center justify-center bg-brand-blue-transparent ">
-      <div className="rounded bg-white p-4">
+      <div className="rounded bg-white p-6">
         <div className="mb-4 flex justify-between">
-          <p className=" text-sm font-bold text-brand-grey-dark">
+          <p className=" text-sm  font-bold text-brand-grey-dark">
             Register your interest
           </p>
           <div>X</div>
         </div>
-        <p>Complete the form below to start your registration process.</p>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div>
+        <p className="mb-4 font-light">
+          Complete the form below to start your registration process.
+        </p>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col items-center"
+        >
+          <div className="my-5 flex gap-5 ">
             <input
               {...register("firstName", { required: true })}
               placeholder="First name"
+              className="active: w-80 border-b border-brand-grey-dark pb-1 font-light focus:border-brand-green focus:outline-none"
             />
             <input
               {...register("lastName", { required: true })}
               placeholder="Last name"
+              className="w-80 border-b border-brand-grey-dark pb-1 font-light focus:border-brand-green focus:outline-none"
             />
           </div>
-          <input type="submit" value="Submit your registration" />
+          <div className="my-5 flex gap-5">
+            <input
+              {...register("email", { required: true })}
+              placeholder="Email address"
+              className="w-80 border-b border-brand-grey-dark pb-1 font-light focus:border-brand-green focus:outline-none"
+            />
+            <input
+              {...register("phone", { required: true })}
+              placeholder="Phone Number"
+              className="w-80 border-b border-brand-grey-dark pb-1 font-light focus:border-brand-green focus:outline-none"
+            />
+          </div>
+          <input
+            type="submit"
+            value="Send your registration"
+            className="w-fit rounded bg-brand-green py-3 px-8 text-white"
+          />
         </form>
       </div>
     </div>
