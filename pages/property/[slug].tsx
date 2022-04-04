6069efc3ae7967bd60d6032ce26cb9ae4a999633
Joe <joe@creativefolks.co.uk>
2022-04-04 10:27:07 +0100

    add areYouInterested component to property page and reduce container size

--- pages/property/[slug].tsx
+++ pages/property/[slug].tsx
@@ -1,18 +1,19 @@
-import About from "components/propertyDetails/About";
-import Accordion from "components/propertyDetails/accordion";
-import Announcement from "components/propertyDetails/announcement/Large";
-import AnnouncementSmall from "components/propertyDetails/announcement/Small";
-import Gallery from "components/propertyDetails/gallery";
-import Header from "components/propertyDetails/Header";
-import HowItWorks from "components/propertyDetails/howItWorks";
-import KeyFeatures from "components/propertyDetails/keyFeatures";
-import Pricing from "components/propertyDetails/pricing";
-import RelatedProperties from "components/propertyDetails/RelatedProperties";
-import mainAccordionData from "../../components/propertyDetails/accordion/mainAccordionData";
+import About from "components/propertyDetails/About"
+import Accordion from "components/propertyDetails/accordion"
+import Announcement from "components/propertyDetails/announcement/Large"
+import AnnouncementSmall from "components/propertyDetails/announcement/Small"
+import Gallery from "components/propertyDetails/gallery"
+import Header from "components/propertyDetails/Header"
+import HowItWorks from "components/propertyDetails/howItWorks"
+import KeyFeatures from "components/propertyDetails/keyFeatures"
+import Pricing from "components/propertyDetails/pricing"
+import RelatedProperties from "components/propertyDetails/RelatedProperties"
+import AreYouInterested from "components/Sidebar/AreYouInterested"
+import mainAccordionData from "../../components/propertyDetails/accordion/mainAccordionData"
 
 function PropertyDetails() {
   return (
-    <div className="container  py-5">
+    <div className="container-sm py-5">
       <Announcement />
       <Header />
       <div className="grid grid-cols-1 gap-5 lg:grid-cols-[auto_400px]">
@@ -27,16 +28,13 @@ function PropertyDetails() {
         </main>
         <aside className="flex flex-col gap-5">
           <Pricing classes="hidden lg:flex" />
-          <div className="flex h-56 items-center justify-center rounded bg-brand-green">
-            {" "}
-            Interested?
-          </div>
+          <AreYouInterested />
           <HowItWorks />
           <AnnouncementSmall />
         </aside>
       </div>
     </div>
-  );
+  )
 }
 
-export default PropertyDetails;
+export default PropertyDetails
