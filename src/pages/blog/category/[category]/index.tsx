6cb8e3c8ffef97fca669c96a5c77ada868df77d9
Joe Cox <joe@creativefolks.co.uk>
2022-04-22 16:39:51 +0100

    category page fully static rendered using ISR, sort based on query params

--- src/pages/blog/category/[category]/index.tsx
+++ src/pages/blog/category/[category]/index.tsx
@@ -3,10 +3,11 @@ import ArticlesFilters from "components/Blog/ArticlesFilters"
 import BlogLink from "components/Blog/BlogLink"
 import BackButton from "components/Core/BackButton"
 import { request } from "lib/datocms"
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from "next"
 import Image from "next/image"
 import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
 import { BlogArticlePreview } from "types/blog"
-import { createArrayBetweenTwoNumbers } from "utils"
 
 interface Props {
   articles: BlogArticlePreview[]
@@ -15,13 +16,42 @@ interface Props {
 }
 
 // show all articles related to the specific type
-// each article will link to just /blog/article-slug, not /blog/[type]/article-slug
-export default function ArticleCategory({ category, articles, count }: Props) {
+// each article will link to just /blog/article-slug, not /blog/[category]/article-slug
+
+// Just to note we are going to sort using query params but client side
+// 1. because it's already built in to ArticleFilters component for a page where getServerSideProps is required
+// 2. because we want people to be able to come to the page potentially with a url that may have query params
+//    so they can view exactly what the original user did i.e. count=2 (show 2 * 9 posts total), sortBy=ASC (show oldest first)
+export default function ArticleCategory({ category, articles }: Props) {
   const router = useRouter()
+  // use a count state variable and with each load more, check if it is equal to numOfPages, if so, remove load more button
+  const [count, setCount] = useState<number>(1)
 
-  const numOfPages = Math.ceil(count / 9)
+  useEffect(() => {
+    if (router.query.count) {
+      setCount(Number(router.query.count))
+    }
+  }, [router.query])
 
-  const pages = createArrayBetweenTwoNumbers(1, numOfPages)
+  const sortBy = router.query.sortBy
+
+  // calculate the number of blocks of 9 articles there would be if you were to show 9 at a time
+  // we will show 9 initially then load more.
+  const numberOfBlocksOfNine = Math.ceil(articles.length / 9)
+
+  // in our query to dato we request articles in DESC order based on createdat date.
+  // here we will sort using the ArticlesFilters functionality which will push a sortBy query to the url
+  // if the sortBy is ASC we can sort by the date client side, otherwise we will just use the articles as they are
+  const sortedArticles =
+    sortBy === "ASC"
+      ? articles.sort(
+          (a, b) =>
+            Number(new Date(a.createdAt)) - Number(new Date(b.createdAt))
+        )
+      : articles
+
+  // get the first 9 articles. Each time count updates we want to change the number of articles based on 9 articles * count
+  const visibleArticles = sortedArticles.slice(0, count * 9)
 
   return (
     <main className="pb-16">
@@ -36,7 +66,7 @@ export default function ArticleCategory({ category, articles, count }: Props) {
       </section>
       <section className="">
         <div className="container-sm flex flex-wrap">
-          {articles.map((article) => (
+          {visibleArticles.map((article) => (
             <article
               className="w-full sm:w-1/2 md:p-3 lg:w-1/3"
               key={article.id}
@@ -69,30 +99,46 @@ export default function ArticleCategory({ category, articles, count }: Props) {
           ))}
         </div>
       </section>
+      {count < numberOfBlocksOfNine && (
+        <section className="py-8">
+          <div className="container-sm">
+            <button
+              className="button mx-auto disabled:bg-gray-300"
+              onClick={() =>
+                router.push(
+                  { query: { ...router.query, count: count + 1 } },
+                  undefined,
+                  { scroll: false }
+                )
+              }
+              disabled={count >= numberOfBlocksOfNine}
+            >
+              Load More Posts ({numberOfBlocksOfNine})
+            </button>
+          </div>
+        </section>
+      )}
     </main>
   )
 }
 
-// hmm, should probably do this as static paths, and just have all articles on the page, showing only 9 first
-// sort order then done client side as well as loading more....
+// articles in this category will all be statically rendered beforehand anyway so there should be no requests to dato other than initial build
+// it is set up for ISR though so if a new article is added the page will need to be reloaded to build and show the updates
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const category = params?.category
 
-export async function getServerSideProps({ query }: any) {
-  const category: string = query.category
-  const sortBy: string =
-    query.sortBy !== ("" || "ASC" || "DESC") ? "DESC" : query.sortBy
-
-  const CATEGORY_ARTICLES = getCategoryArticles(0, category, sortBy)
+  const CATEGORY_ARTICLES = getCategoryArticles(category)
 
   try {
     const data = await request({
       query: CATEGORY_ARTICLES,
       endpoint: process.env.NEXT_DATO_ENVIRONMENT,
-    } as any)
+    })
 
     const articles = data.allArticles
-    const count = data._allArticlesMeta.count
     return {
-      props: { category: category, articles, count },
+      props: { category: category, articles },
+      revalidate: 10,
     }
   } catch (error) {
     return {
@@ -101,16 +147,35 @@ export async function getServerSideProps({ query }: any) {
   }
 }
 
-export const getCategoryArticles = (
-  skip: number = 0,
-  category: string,
-  sortOrder: string = "DESC"
-) => {
-  return `query BlogHomeArticles {
-    _allArticlesMeta(filter: {_status: {eq: published}, category: { matches: {pattern: "${category}"} } }) {
-      count
+// get all the existing category paths
+export const getStaticPaths: GetStaticPaths = async () => {
+  const CATEGORIES = `query ArticleCategories {
+    allArticles {
+      category
     }
-    allArticles(filter: {_status: {eq: published}, category: {matches: {pattern: "${category}"} } }, orderBy: _createdAt_${sortOrder}, skip: "${skip}", first: "5") {
+  }`
+
+  const data = await request({
+    query: CATEGORIES,
+    endpoint: process.env.NEXT_DATO_ENVIRONMENT,
+  })
+
+  const paths: { params: { category: string } }[] = []
+
+  data.allArticles.forEach((article: { category: string }) => {
+    paths.push({ params: { category: article.category.toLowerCase() } })
+  })
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+// get articles by category. This will be fed in to static props so all articles can be requested and built on build
+export const getCategoryArticles = (category?: string | string[]) => {
+  return `query BlogHomeArticles {
+    allArticles(filter: {_status: {eq: published}, category: {matches: {pattern: "${category}"} } }, orderBy: _createdAt_DESC) {
       mainImage {
         alt
         url
