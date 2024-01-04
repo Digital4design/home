8240f51f3f72884bf8b436a95bafc380f7804f65
ravid4d <ravichaudharyd4d@gmail.com>
2024-01-04 16:40:50 +0530

    change

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document"
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>{/* any stylesheet links, shortcut icon, icons here */}</Head>
+      <Head>{/* any stylesheet links, shortcut icon, icons here */} </Head>
       <body>
         <Main />
         <NextScript />
